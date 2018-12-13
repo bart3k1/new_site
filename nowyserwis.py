e9c11941e8c555a5e8303e553b3662acb2652a25
bart3k1 <bart3k1@gmail.com>
2018-12-13 18:29:22 +0100

    translation and file management added

--- nowyserwis.py
+++ nowyserwis.py
@@ -1,8 +1,10 @@
 import docx2txt
 import os
-import webbrowser
-# from googletrans import Translator
-from py_translator import Translator
+from translate.translate import Translator
+
+# import webbrowser
+
+
 
 # set chrome path
 chrome_path = '/usr/bin/google-chrome %s'
@@ -13,47 +15,65 @@ directory = "/home/satq/Documents/Pythony/new_site/doce"
 # set list of title tags
 tag_list = ("T: ", "Title:", "Tytuł:")
 
+# set file list
+file_list = []
+
 # set title list
 title_list = []
+
+# set translator languages
+translator = Translator(to_lang='en', from_lang='pl')
+
+# set title list
+translated_title_list = []
+
+try:
+    os.stat(directory + '/used')
+except:
+    os.mkdir(directory + '/used')
 
 # browse files
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     if filename.endswith(".docx"):
-        # print(os.path.join(directory, filename))
-
+        # file_list.append((directory + "/used/" + filename))
+        file_list.append(filename)
         # read .docx files
         my_text = docx2txt.process(directory + "/" + file)
         my_text_XXX = my_text.replace('\n', 'XXX')
         my_text_split = my_text_XXX.split('XXX')
 
-        # check for title
+        # check for title add to title_list and translate_title_list
         for line in my_text_split:
             if line.startswith(tag_list):
-                title_list.append(line)
-        continue
+                start_char = line.find(':')
+                print(start_char)
+                title_list.append(line[7:]) #check if all stats at 7th todo
+                # TRANSLATIONS LIMIT todo
+                # translation = translator.translate(line)
+                # translated_title_list.append(translation)
+
+
     else:
-        continue
+        pass
 
-print(title_list)
+    f = open("spis.txt", "a+")
+    f.write(title_list[-1] + "\n")
+    # f.write(translated_title_list[-1] + "\n")
+    f.write(file_list[-1] + "\n\n")
+    f.close()
 
-# translate - webpage
+    #  move done file -> used todo
+    # shutil.move(directory + "/" + file, directory + "/used/" + file)
 
-# for i in title_list:
-#     j = i.replace(" ", "%20")
-#     print(j)
-#     webbrowser.get(chrome_path).open("https://translate.google.pl/#view=home&op=translate&sl=auto&tl=en&text=" + j)
+a = 0
+for i in title_list:
+    print(i)
+    print(file_list[a])
+    a += 1
 
-# translate googletrans/py_translator
-# translator = Translator()
-#
-# translated = translator.translate("dupa", dest='en')
-# print(translated.text)
 
-# s = Translator().translate('Hello my friend', dest='es').text
-# print(s)
 
-# for i in title_list:
-#     translated = translator.translate("dupa")
-#     print(i)
-#     print(translated.text)
+
+
+
