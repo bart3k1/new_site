23b926c42c530e6366b6f1d1460b5b0e85c43e02
bart3k1 <bart3k1@gmail.com>
2018-12-14 18:45:48 +0100

    file import started

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, render_template, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from werkzeug import secure_filename
 
 app = Flask(__name__)
 
@@ -15,3 +16,18 @@ def index():
     articles = db.execute("SELECT * FROM articles").fetchall()
     return render_template("index.html", articles=articles)
 
+
+@app.route('/upload')
+def upload_file():
+    return render_template('upload.html')
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file1():
+    if request.method == 'POST':
+        f = request.files['file']
+        # f = open('masters.txt')
+        for line in f:
+            print(line)
+        f.close()
+        return 'file uploaded successfully'
